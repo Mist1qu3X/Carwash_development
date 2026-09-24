a50b229ea0ec92779d1d8f6b62e026cf4a5f2b28
Сергей Коржов <korzhov.s.s@edu.mirea.ru>
2026-09-24 15:20:17 +0300

    Составил структуру проекта, распределил папки и файлы, добавил слой допступа к БД, и также добавлены исключения

---
+++ src/main/java/carwash/repository/Repository.java
@@ -0,0 +1,18 @@
+package carwash.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+// общий интерфейс репозитория (CRUD), реализуют ClientRepository и BookingRepository
+public interface Repository<T> {
+
+    T save(T entity);
+
+    List<T> findAll();
+
+    Optional<T> findById(int id);
+
+    void update(T entity);
+
+    void deleteById(int id);
+}
